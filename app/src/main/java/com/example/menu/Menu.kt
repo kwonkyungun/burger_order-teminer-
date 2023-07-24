417ccbfbf1fa5acdd763d5927f8d71529169a0b6
Lim-Juri <imjuly05@gmail.com>
2023-07-24 20:03:24 +0900

    SideMenu 추가 파일

--- app/src/main/java/com/example/menu/Menu.kt
+++ app/src/main/java/com/example/menu/Menu.kt
@@ -1,19 +1,22 @@
 package com.example.menu
 
-fun main() {
-        println("1번: [햄버거메뉴] 2번: [사이드메뉴] 3번: [음료메뉴] 4번: [종료]")
+fun main (){
+        println("========================Welcome=========================")
+        println("[1] Burger Menu, [2] Side Menu, [3] Drink Menu, [4] 종료")
+
         var select = readLine()!!.toInt()
-        if (select == 1) {
+        if (select == 1){
                 println("햄버거메뉴입니다.")
-        } else if (select == 2) {
+        }else if(select == 2){
                 println("사이드메뉴입니다.")
-        } else if (select == 3) {
+                SideMenu().sidemenu()
+        }else if (select == 3){
                 println("음료메뉴입니다.")
-        } else if (select == 4) {
+        }else if (select == 4){
                 println("종료합니다.")
-        } else {
+        }
+        else {
                 println("잘못된 정보입니다.")
         }
-        fun Drink() {}
 }
 
