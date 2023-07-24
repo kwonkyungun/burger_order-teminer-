0b35d551c3ec67fb763e06874e60619bf344c7ff
kku <ruddns5315@gmail.com>
2023-07-24 14:41:21 +0900

    second commit

---
+++ Kiosk/app/src/main/java/com/example/kiosk/main.kt
@@ -0,0 +1,10 @@
+package com.example.kiosk
+fun main (){
+    var num1= readLine()!!.toInt()
+    var num2= readLine()!!.toInt()
+    var result = num1/num2
+
+
+    println("${num1}을 ${num2}로 나눈 몫은 ${result} 입니다.")
+
+}
