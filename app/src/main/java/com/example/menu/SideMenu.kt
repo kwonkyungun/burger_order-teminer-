f8046ea9e9220060cdb639554b76e50adde9ef89
Lim-Juri <imjuly05@gmail.com>
2023-07-24 19:49:21 +0900

    SideMenu22

--- app/src/main/java/com/example/menu/SideMenu.kt
+++ app/src/main/java/com/example/menu/SideMenu.kt
@@ -1,5 +1,6 @@
 package com.example.menu
 
+//SideMenu
 class SideMenu {
     fun sidemenu() {
         println("=================================[Side Menu]=============================================")
