de17cf301a2695fccdedd0245dfa5f968d144e36
Lim-Juri <imjuly05@gmail.com>
2023-07-24 15:02:54 +0900

    메뉴판

---
+++ app/src/main/java/com/example/menu/Menu.kt
@@ -0,0 +1,7 @@
+package com.example.menu
+
+fun main() {
+        println("========================Welcome=========================")
+        println("[1] Burger Menu, [2] Side Menu, [3] Drink Menu, [4] 종료")
+
+}
