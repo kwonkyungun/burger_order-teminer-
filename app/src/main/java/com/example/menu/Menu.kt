72e08600a887cd09350d4b3401fd69c14012b0f1
Martin Kwon <mindori9097@gmail.com>
2023-07-24 19:52:26 +0900

    burger

--- app/src/main/java/com/example/menu/Menu.kt
+++ app/src/main/java/com/example/menu/Menu.kt
@@ -1,8 +1,19 @@
 package com.example.menu
 
 fun main() {
-        println("========================Welcome=========================")
-        println("[1] Burger Menu, [2] Side Menu, [3] Drink Menu, [4] 종료")
-
+        println("1번: [햄버거메뉴] 2번: [사이드메뉴] 3번: [음료메뉴] 4번: [종료]")
+        var select = readLine()!!.toInt()
+        if (select == 1) {
+                println("햄버거메뉴입니다.")
+        } else if (select == 2) {
+                println("사이드메뉴입니다.")
+        } else if (select == 3) {
+                println("음료메뉴입니다.")
+        } else if (select == 4) {
+                println("종료합니다.")
+        } else {
+                println("잘못된 정보입니다.")
+        }
+        fun Drink() {}
 }
-//dksakjshdkljahsjd
+
