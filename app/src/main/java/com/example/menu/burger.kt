8e83dcd27e2f6f40fb88140f1740c5f1f3cb78f3
Martin Kwon <mindori9097@gmail.com>
2023-07-25 17:27:04 +0900

    44444

--- app/src/main/java/com/example/menu/burger.kt
+++ app/src/main/java/com/example/menu/burger.kt
@@ -5,29 +5,34 @@ package com.example.menu
 class Hamburger {
     fun burger() {
         println("[ Burgers MENU ]")
-        println("1. ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거")
-        println("2. SmokeShack    | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거")
-        println("3. Shroom Burger | W 9.4 | 몬스터 치즈와 체다 치즈로 속을 채운 베지테리안 버거")
-        println("4. Cheeseburger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거")
-        println("5. Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거")
+        println("1. ShackBurger   | W 6900 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거")
+        println("2. SmokeShack    | W 8900 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거")
+        println("3. ShroomBurger  | W 9400 | 몬스터 치즈와 체다 치즈로 속을 채운 베지테리안 버거")
+        println("4. Cheeseburger  | W 6900 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거")
+        println("5. Hamburger     | W 5400 | 비프패티를 기반으로 야채가 들어간 기본버거")
         println("0. 뒤로가기       | 뒤로가기")
 
         var Burgerselect = readLine()!!.toInt()
 
         if (Burgerselect == 1) {
             println("ShackBurger를 고르셨습니다")
+            println("ShackBurger | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거")
             Ingredients()
         } else if (Burgerselect == 2) {
             println("SmokeShack를 고르셨습니다")
+            println("SmokeShack | W 8900 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거")
             Ingredients()
         } else if (Burgerselect == 3) {
             println("ShroomBurger를 고르셨습니다")
+            println("ShroomBurger | W 9400 | 몬스터 치즈와 체다 치즈로 속을 채운 베지테리안 버거")
             Ingredients()
         } else if (Burgerselect == 4) {
             println("CheeseBurger를 고르셨습니다")
+            println("Cheeseburger | W 6900 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거")
             Ingredients()
         } else if (Burgerselect == 5) {
             println("Hamburger를 고르셨습니다")
+            println("Hamburger | W 5400 | 비프패티를 기반으로 야채가 들어간 기본버거")
             Ingredients()
         } else if (Burgerselect == 0) {
             println("돌아가기")
@@ -42,7 +47,7 @@ class Hamburger {
         var count3 = 1
 
         while (true) {
-            println("[1] 패티추가, [2] 양상추 추가, [3] 소스추가 4.[종료]")
+            println("[1] 패티 추가, [2] 양상추 추가, [3] 쉑소스 추가 4.[종료]")
             var selectNumber = readLine()!!.toInt()
 
             when (selectNumber) {
@@ -59,7 +64,7 @@ class Hamburger {
                 }
 
                 3 -> {
-                    println("소스를 ${count3}번 추가합니다.")
+                    println("쉑소스를 ${count3}번 추가합니다.")
                     count3++
 
                 }
---
+++ app/src/main/java/com/example/menu/cashshop.kt
@@ -0,0 +1,78 @@
+package com.example.menu
+
+class CashShop private constructor() {
+    private val ShackBurgerPrice = 6900
+    private val SmokeBurgerPrice = 8900
+    private val ShroomBurgerPrice = 9400
+    private val CheeseBurgerPrice = 6900
+    private val HamburgerPrice = 5400
+
+    companion object {
+        @Volatile
+        private var instance: CashShop? = null
+
+        fun getInstance(): CashShop {
+            // 외부에서 요청왔을때 instance가 null인지 검증
+            if (instance == null) {
+                // synchronized로 외부 쓰레드의 접근을 막음
+                // 쓰레드는 다음챕터에서 소개합니다!
+                // 쓰레드간의 객체상태 혼돈을 막기위해 사용한다고 이해해주세요
+                synchronized(this) {
+                    instance = CashShop()
+                }
+            }
+            return instance!!
+        }
+    }
+
+    fun purchaseShackBurgerByCustomer(character: Customer) {
+        if (character.money >= ShackBurgerPrice) {
+            println("[구매 후 금액]: [${character.money} - ${ShackBurgerPrice}] = ${character.money - ShackBurgerPrice}")
+            character.money -= ShackBurgerPrice
+            character.shoppinglist.add("ShackBurger를 구매합니다")
+        } else {
+            println("돈이 부족합니다.")
+        }
+    }
+
+    fun purchaseSmokeBurgerPriceByCustomer(character: Customer) {
+        if (character.money >= SmokeBurgerPrice) {
+            println("[구매 후 금액]: [${character.money} - ${SmokeBurgerPrice}] = ${character.money - SmokeBurgerPrice}")
+            character.money -= SmokeBurgerPrice
+            character.weapons.add("SmokeBurger를 구매합니다")
+        } else {
+            println("돈이 부족합니다.")
+        }
+    }
+
+    fun purchaseShroomBurgerPriceByCustomer(character: Customer) {
+        if (character.money >= ShroomBurgerPrice) {
+            println("[구매 후 금액]: [${character.money} - ${ShroomBurgerPrice}] = ${character.money - ShroomBurgerPrice}")
+            character.money -= ShroomBurgerPrice
+            character.shoppinglist.add("ShroomBurger를 구매합니다")
+        } else {
+            println("돈이 부족합니다.")
+        }
+    }
+
+    fun purchaseCheeseBurgerPriceByCustomer(character: Customer) {
+        if (character.money >= CheeseBurgerPrice) {
+            println("[구매 후 금액]: [${character.money} - ${CheeseBurgerPrice}] = ${character.money - CheeseBurgerPrice}")
+            character.money -= CheeseBurgerPrice
+            character.shoppinglist.add("CheeseBurger를 구매합니다")
+        } else {
+            println("돈이 부족합니다.")
+        }
+    }
+
+    fun purchaseHamburgerPriceByCustomer(character: Customer) {
+        if (character.money >= HamburgerPrice) {
+            println("[구매 후 금액]: [${character.money} - ${HamburgerPrice}] = ${character.money - HamburgerPrice}")
+            character.money -= HamburgerPrice
+            character.shoppinglist.add("Hamburger를 구매합니다")
+        } else {
+            println("돈이 부족합니다.")
+
+        }
+    }
+}
