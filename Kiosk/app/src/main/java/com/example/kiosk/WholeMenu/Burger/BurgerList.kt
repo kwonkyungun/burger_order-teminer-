ee4843e7c1b70fe8be48b5d37719091c17187859
Martin Kwon <mindori9097@gmail.com>
2023-07-27 14:43:17 +0900

    BurgerList

--- Kiosk/app/src/main/java/com/example/kiosk/WholeMenu/Burger/BurgerList.kt
+++ Kiosk/app/src/main/java/com/example/kiosk/WholeMenu/Burger/BurgerList.kt
@@ -6,7 +6,7 @@ import com.example.kiosk.InitMenu
 class BurgerList {
     fun burger() {
         println(
-            "[ Burgers MENU ] \n" +
+                    "[ Burgers MENU ] \n" +
                     "1. ShackBurger   | W 6900 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거 \n" +
                     "2. SmokeShack    | W 8900 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거 \n" +
                     "3. ShroomBurger  | W 9400 | 몬스터 치즈와 체다 치즈로 속을 채운 베지테리안 버거 \n" +
