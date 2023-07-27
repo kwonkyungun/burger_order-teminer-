c2e9a9a8732a313fd4405974ad494e744924d981
Lim-Juri <imjuly05@gmail.com>
2023-07-27 12:02:06 +0900

    SideMenu 추가했습니다ㅏㅏ

--- Kiosk/app/src/main/java/com/example/kiosk/WholeMenu/Side/SideList.kt
+++ Kiosk/app/src/main/java/com/example/kiosk/WholeMenu/Side/SideList.kt
@@ -1,4 +1,91 @@
 package com.example.kiosk.WholeMenu.Side
 
+import com.example.kiosk.InitMenu
+import com.example.kiosk.WholeMenu.InitMenu
+
 class SideList {
+    fun sidemenu() {
+        println("[ Burgers MENU ]")
+        println("1. Chicken Tenders  | W 4500 | 100% 국내산 닭다리 통살로 튀겨진 치킨텐더")
+        println("2. French Fries     | W 3000 | 인기만점 바삭한 프렌치 프라이")
+        println("3. Cheese Stick     | W 2000 | 통모짜렐라치즈에 튀김옷을 입혀 만든 바삭하고 고소한 대표 디저트 메뉴")
+        println("0. 뒤로가기           | 뒤로가기")
+
+        var sideselect = readLine()!!.toInt()
+        var count1 = 1
+        var count2 = 1
+        var count3 = 1
+        if (sideselect == 1) {
+            println("Chicken Tenders를 고르셨습니다")
+            println("Chicken Tenders  | W 4500 | 100% 국내산 닭다리 통살로 튀겨진 치킨텐더")
+            add()
+            count1++
+        } else if (sideselect == 2) {
+            println("French Fries를 고르셨습니다")
+            println("French Fries     | W 3000 | 인기만점 바삭한 프렌치 프라이")
+            add()
+            count2++
+        } else if (sideselect == 3) {
+            println("Cheese Stick을 고르셨습니다")
+            println("Cheese Stick     | W 2000 | 통모짜렐라치즈에 튀김옷을 입혀 만든 바삭하고 고소한 대표 디저트 메뉴")
+            add()
+            count3++
+        } else if (sideselect == 0) {
+            println("뒤로가기")
+            InitMenu().mainMenu()
+        } else {
+            println("잘못입력하였습니다.")
+            sidemenu()
+        }
+    }
 }
+
+fun add() {
+
+    println("[ Add Side Option ]")
+    println("1. Chicken Tenders  | W 4500 | 치킨텐더 추가하기")
+    println("2. French Fries     | W 3000 | 프렌치 프라이 추가하기")
+    println("3. Cheese Stick     | W 2000 | 치즈스틱 추가하기")
+    println("4. 추가안함           | 뒤로가기")
+    while (true) {
+
+        var selectNumber = readLine()!!.toInt()
+
+        when (selectNumber) {
+            1 -> {
+                println("치킨텐더를 ${count1}개 | W 4500 | 추가합니다.")
+                count1++
+
+            }
+
+            2 -> {
+                println("프렌치 프라이를 ${count2}개 | W 3000 | 추가합니다.")
+                count2++
+
+            }
+
+            3 -> {
+                println("치즈스틱을 ${count3}개 | W 2000 | 추가합니다.")
+                count3++
+
+            }
+
+            0 -> {
+                println("뒤로가기")
+            }
+
+            else -> {
+                println("잘못입력하였습니다.")
+                add()
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
--- Kiosk/app/src/main/java/com/example/kiosk/main.kt
+++ Kiosk/app/src/main/java/com/example/kiosk/main.kt
@@ -6,7 +6,6 @@ fun main() {
     val myMoney = initmoney("money").toString().toInt()
     initmoney(type = String.toString())
 
-    initmenu()
 
 }
 
@@ -29,84 +28,84 @@ fun initmoney(type: String): Any? {
     }
 }
 
-fun initmenu() {
-
-    println("1번: [햄버거메뉴] 2번: [사이드메뉴] 3번: [음료메뉴] 4번: [종료]")
-    var select = readLine()!!.toInt()
-    if (select == 1) {
-        println("햄버거메뉴입니다.")
-    } else if (select == 2) {
-        println("사이드메뉴입니다.")
-    } else if (select == 3) {
-        println("음료메뉴입니다.")
-        classDrink().Beverage()
-    } else if (select == 4) {
-        println("종료합니다.")
-
-    } else {
-        println("잘못된 정보입니다.")
-        initmenu()
-    }
-}
-
-class classDrink {
-    fun Beverage() {
-
-        println("1번: [콜라] 2번: [사이다] 3번: [환타] 4번:[미란다] 5번:[뒤로가기]")
-        var Drinkselect = readLine()!!.toInt()
-        var count1 = 1
-        var count2 = 1
-        var count3 = 1
-        var count4 = 1
-        if (Drinkselect == 1) {
-            count1++
-        } else if (Drinkselect == 2) {
-            count2++
-        } else if (Drinkselect == 3) {
-            count3++
-        } else if (Drinkselect == 4) {
-            count4++
-        } else if(Drinkselect == 5) {
-            initmenu()
-        } else {
-            println("잘못입력하였습니다.")
-        }
-
-
-        while (true) {
-            println("[1] 콜라 추가 [2] 사이다 추가 [3] 환타 추가 [4] 미란다 추가 [5]뒤로가기")
-            var selectNumber = readLine()!!.toInt()
-
-            when (selectNumber) {
-                1 -> {
-                    println("콜라를 ${count1}잔 추가합니다.")
-                    count1++
-                }
-
-                2 -> {
-                    println("사이다를 ${count2}잔 추가합니다.")
-                    count2++
-                }
-
-                3 -> {
-                    println("환타를 ${count3}잔 추가합니다.")
-                    count3++
-                }
-
-                4 -> {
-                    println("미란다를 ${count3}잔 추가합니다.")
-                    count4++
-                }
-
-                else -> {
-                    println("메인메뉴로 돌아갑니다.")
-                    break
-                }
-            }
-        }
-        initmenu()
-    }
-}
+//fun initmenu() {
+//
+//    println("1번: [햄버거메뉴] 2번: [사이드메뉴] 3번: [음료메뉴] 4번: [종료]")
+//    var select = readLine()!!.toInt()
+//    if (select == 1) {
+//        println("햄버거메뉴입니다.")
+//    } else if (select == 2) {
+//        println("사이드메뉴입니다.")
+//    } else if (select == 3) {
+//        println("음료메뉴입니다.")
+//        classDrink().Beverage()
+//    } else if (select == 4) {
+//        println("종료합니다.")
+//
+//    } else {
+//        println("잘못된 정보입니다.")
+//        initmenu()
+//    }
+////}
+//
+//class classDrink {
+//    fun Beverage() {
+//
+//        println("1번: [콜라] 2번: [사이다] 3번: [환타] 4번:[미란다] 5번:[뒤로가기]")
+//        var Drinkselect = readLine()!!.toInt()
+//        var count1 = 1
+//        var count2 = 1
+//        var count3 = 1
+//        var count4 = 1
+//        if (Drinkselect == 1) {
+//            count1++
+//        } else if (Drinkselect == 2) {
+//            count2++
+//        } else if (Drinkselect == 3) {
+//            count3++
+//        } else if (Drinkselect == 4) {
+//            count4++
+//        } else if(Drinkselect == 5) {
+//            initmenu()
+//        } else {
+//            println("잘못입력하였습니다.")
+//        }
+//
+//
+//        while (true) {
+//            println("[1] 콜라 추가 [2] 사이다 추가 [3] 환타 추가 [4] 미란다 추가 [5]뒤로가기")
+//            var selectNumber = readLine()!!.toInt()
+//
+//            when (selectNumber) {
+//                1 -> {
+//                    println("콜라를 ${count1}잔 추가합니다.")
+//                    count1++
+//                }
+//
+//                2 -> {
+//                    println("사이다를 ${count2}잔 추가합니다.")
+//                    count2++
+//                }
+//
+//                3 -> {
+//                    println("환타를 ${count3}잔 추가합니다.")
+//                    count3++
+//                }
+//
+//                4 -> {
+//                    println("미란다를 ${count3}잔 추가합니다.")
+//                    count4++
+//                }
+//
+//                else -> {
+//                    println("메인메뉴로 돌아갑니다.")
+//                    break
+//                }
+//            }
+//        }
+//        initmenu()
+//    }
+//}
 //    if (Drinkselect == 1) {
 //        println("콜라를 선택하였습니다.")
 //        val num1 = "콜라"
