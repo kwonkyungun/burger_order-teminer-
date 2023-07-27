738044c1d118cba0ffcb8c4be4f2c21816ac589a
Martin Kwon <mindori9097@gmail.com>
2023-07-27 11:31:11 +0900

    BurgerList

--- Kiosk/app/src/main/java/com/example/kiosk/WholeMenu/Burger/BurgerList.kt
+++ Kiosk/app/src/main/java/com/example/kiosk/WholeMenu/Burger/BurgerList.kt
@@ -4,13 +4,15 @@ import com.example.kiosk.initmenu
 
 class BurgerList {
     fun burger() {
-        println("[ Burgers MENU ] \n" +
-                "1. ShackBurger   | W 6900 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거 \n" +
-                "2. SmokeShack    | W 8900 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거 \n" +
-                "3. ShroomBurger  | W 9400 | 몬스터 치즈와 체다 치즈로 속을 채운 베지테리안 버거 \n" +
-                "4. Cheeseburger  | W 6900 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거 \n" +
-                "5. BasicBurger   | W 5400 | 비프패티를 기반으로 야채가 들어간 기본버거 \n" +
-                "0. 뒤로가기       | 뒤로가기 \n")
+        println(
+            "[ Burgers MENU ] \n" +
+                    "1. ShackBurger   | W 6900 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거 \n" +
+                    "2. SmokeShack    | W 8900 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거 \n" +
+                    "3. ShroomBurger  | W 9400 | 몬스터 치즈와 체다 치즈로 속을 채운 베지테리안 버거 \n" +
+                    "4. Cheeseburger  | W 6900 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거 \n" +
+                    "5. BasicBurger   | W 5400 | 비프패티를 기반으로 야채가 들어간 기본버거 \n" +
+                    "0. 뒤로가기       | 뒤로가기 \n"
+        )
 
         var Burgerselect = readLine()!!.toInt()
         var count1 = 1
@@ -19,42 +21,38 @@ class BurgerList {
         var count4 = 1
         var count5 = 1
         if (Burgerselect == 1) {
-            println("ShackBurger를 고르셨습니다 \n"+
-                    "ShackBurger | W 6900 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거 \n")
-
-            Ingredients()
+            println("ShackBurger를 고르셨습니다")
+            println("ShackBurger | W 6900 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거 ")
+            ingredients()
             count1++
         } else if (Burgerselect == 2) {
-            println("SmokeShack를 고르셨습니다 \n"+
-                    "SmokeShack | W 8900 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거 \n")
-            println()
-            //Shoppinglist()
-            Ingredients()
+            println("SmokeShack를 고르셨습니다")
+            println("SmokeShack | W 8900 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거")
+            ingredients()
             count2++
         } else if (Burgerselect == 3) {
-            println("ShroomBurger를 고르셨습니다 \n"+
-                    "ShroomBurger | W 9400 | 몬스터 치즈와 체다 치즈로 속을 채운 베지테리안 버거 \n"
-            println()
-            Ingredients()
+            println("ShroomBurger를 고르셨습니다")
+            println("ShroomBurger | W 9400 | 몬스터 치즈와 체다 치즈로 속을 채운 베지테리안 버거")
+            ingredients()
             count3++
         } else if (Burgerselect == 4) {
             println("CheeseBurger를 고르셨습니다")
             println("Cheeseburger | W 6900 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거")
-            Ingredients()
+            ingredients()
             count4++
         } else if (Burgerselect == 5) {
             println("BasicBurger를 고르셨습니다")
             println("BasicBurger | W 5400 | 비프패티를 기반으로 야채가 들어간 기본버거")
-            Ingredients()
+            ingredients()
             count5++
         } else if (Burgerselect == 0) {
             println("뒤로가기")
-            initmenu()
+            mainMenu()
         }
     }
 
 
-    fun Ingredients() {
+    fun ingredients() {
         println("[ Add Ingredients ]")
         println("1. Patty        | W 1500 | 패티 한 장 추가하기")
         println("2. Lettuce      | W 1000 | 양상추 추가하기")
@@ -93,6 +91,6 @@ class BurgerList {
                 }
             }
         }
-        initmenu()
+        mainMenu()
     }
 }
